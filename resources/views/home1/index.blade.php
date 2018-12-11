04c92e956e78aad0d158187aa604cec24d55caea
Diki <dikydharmawan62@gmail.com>
2018-12-11 11:57:32 +0700

    template

---
+++ resources/views/home1/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.admin_view')	
+@section('content')
+<H1>Tesst</H1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('home1','Home1Controller@index')->name('home1');
+
+;
