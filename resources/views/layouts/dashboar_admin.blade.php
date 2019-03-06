ebd0f569ce4eb843e839a8301e54bccf95c821df
Lufianti <43260460+Lufianti@users.noreply.github.com>
2019-03-06 09:58:23 +0700

    update

--- resources/views/layouts/dashboar_admin.blade.php
+++ resources/views/layouts/dashboar_admin.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.admin_view')
 @section('content')
-<div class="content-wrapper">
+<div class="container">
   <section class="content-header">
-    <div id="content-wrapper">
+
       <script type="text/javascript">
       (function($){
         $(function(){
@@ -11,60 +11,64 @@
       })(jQuery);
 
       </script>
-          <div class="container-fluid">
+          <div class="col-11">
               <!-- Breadcrumbs-->
               <ol class="breadcrumb">
                 <li class="breadcrumb-item">
                   <a href="#">Dashboard</a>
                 </li>
               </ol>
-      <div class="row">
+              <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-yellow">
+                  <div class="inner">
+                    <h3>3</h3>
+                    <p>Role Management</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                  </div>
+                  <a href="{{route('role.index')}}" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                  <div class="inner">
+                    <h3>3</h3>
+                    <p>User Management</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                  </div>
+                  <a href="{{route('user.data')}}" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-red">
+                  <div class="inner">
+                    <h3>1</h3>
+                    <p>Data Vendor</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                  </div>
+                  <a href="{{ route('vendor.index') }}" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                  </a>
+                </div>
+              </div>
+      <div class="col-xs-12">
         <!-- ./col -->
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-          <div class="inner">
-            <h3>3</h3>
-            <p>Role Management</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-person-add"></i>
-          </div>
-          <a href="{{route('role.index')}}" class="small-box-footer">
-            More info <i class="fa fa-arrow-circle-right"></i>
-          </a>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3>3</h3>
-            <p>User Management</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-person-add"></i>
-          </div>
-          <a href="{{route('user.data')}}" class="small-box-footer">
-            More info <i class="fa fa-arrow-circle-right"></i>
-          </a>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-red">
-          <div class="inner">
-            <h3>1</h3>
-            <p>Data Vendor</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-person-add"></i>
-          </div>
-          <a href="{{ route('vendor.index') }}" class="small-box-footer">
-            More info <i class="fa fa-arrow-circle-right"></i>
-          </a>
-        </div>
-      </div>
+    </div>
+  </div>
+</div>
+</div>
 @endsection
